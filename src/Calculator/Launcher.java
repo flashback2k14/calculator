a70f457a4ed7302bccd969f212a3f25ae945cdbf
cs13dr1 <guinnes83@googlemail.com>
2015-01-15 20:36:38 +0100

    MVC

--- src/Calculator/Launcher.java
+++ src/Calculator/Launcher.java
@@ -15,50 +15,13 @@ public class Launcher {
 	
 	public static void main(String[] args) {
 		
-		Controller c = new Controller();
+		//Controller c = new Controller();
 		View v = new View();
-		
-		JFrame frame = new JFrame();
-		JButton button1 = new JButton("Light/Dark");
-		JButton button2 = new JButton("About");
-		JPanel tastenfeld = new JPanel();
-		JTextField anzeigeFeld = new JTextField(30);
-		
-		GridLayout grid = new GridLayout(4,4);
-		tastenfeld.setLayout(grid);
-		
-		String[] labelBezeichnung = {"1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "=", "0", "Reset", "/"};
-		JButton[] but = new JButton[labelBezeichnung.length];
-		
-		for (int i = 0; i < labelBezeichnung.length; i++) {
-			
-			but[i] = new JButton(labelBezeichnung[i]);
-			but[i].addActionListener(c);
-			but[i].setActionCommand(labelBezeichnung[i]);
-			tastenfeld.add(but[i]);
-			
-		}
-		
+				
 		//Create Panels
 		v.init();
 		
-		frame.add(v.panel2, BorderLayout.NORTH);
-		frame.add(v.panel1, BorderLayout.SOUTH);
-		frame.add(tastenfeld, BorderLayout.CENTER);
 		
-		v.panel1.add(button1);
-		v.panel1.add(button2);
-		
-		button2.addActionListener(c);
-		button2.setActionCommand("about");
-		button1.addActionListener(c);
-		button1.setActionCommand("theme");
-		
-		v.panel2.add(anzeigeFeld);
-		
-		frame.setTitle("Calculator");
-		frame.setSize(800, 600);
-		frame.setVisible(true);
 
 	}
 
--- src/Calculator/View.java
+++ src/Calculator/View.java
@@ -1,23 +1,74 @@
 package Calculator;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class View {
 
 	JPanel panel1;
 	JPanel panel2;
+	
 	int id = 0;
 	
 	public void init() {
+		
+		Controller c = new Controller();
+		
+		JFrame frame = new JFrame();
+		JButton button1 = new JButton("Light/Dark");
+		JButton button2 = new JButton("About");
+		JPanel tastenfeld = new JPanel();
+		JTextField anzeigeFeld = new JTextField(30);
+		
+		GridLayout grid = new GridLayout(4,4);
+		tastenfeld.setLayout(grid);
+		
+		// Create Buttons
+		String[] labelBezeichnung = {"1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "=", "0", "Reset", "/"};
+		JButton[] but = new JButton[labelBezeichnung.length];
+		
+		for (int i = 0; i < labelBezeichnung.length; i++) {
+			
+			but[i] = new JButton(labelBezeichnung[i]);
+			but[i].addActionListener(c);
+			but[i].setActionCommand(labelBezeichnung[i]);
+			tastenfeld.add(but[i]);
+		}
+		
+		// Button Action
+		button1.addActionListener(c);
+		button1.setActionCommand("theme");
+		button2.addActionListener(c);
+		button2.setActionCommand("about");
+		
+		// Create Panels
 		panel1 = new JPanel();
 		panel2 = new JPanel();
-		
 		panel1.setBackground(Color.RED);
 		panel2.setBackground(Color.YELLOW);
-		
 		id = 0;
+		
+		// Add Buttons/TextField to Panels
+		panel1.add(button1);
+		panel1.add(button2);
+		panel2.add(anzeigeFeld);
+		
+		// Add Items to Frame
+		frame.add(panel2, BorderLayout.NORTH);
+		frame.add(panel1, BorderLayout.SOUTH);
+		frame.add(tastenfeld, BorderLayout.CENTER);
+		
+		// Create Frame
+		frame.setTitle("Calculator");
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+		
 	}
 	
 	public void changeTheme() {
@@ -27,12 +78,13 @@ public class View {
 			panel2.setBackground(Color.GREEN);
 			panel1.repaint();
 			panel2.repaint();
+			id = 1;
 		} else {
 			panel1.setBackground(Color.RED);
 			panel2.setBackground(Color.YELLOW);
 			panel1.repaint();
 			panel2.repaint();
-			id = 1;
+			id = 0;			
 		}
 	}
 }
